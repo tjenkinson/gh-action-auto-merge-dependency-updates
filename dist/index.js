0392f798021aadeafe30aea016c3a86e708900aa
Tom Jenkinson <tom@tjenkinson.me>
2020-08-31 10:14:17 +0100

    update build

--- dist/index.js
+++ dist/index.js
@@ -7932,6 +7932,7 @@ function run() {
                                                             repo: context.repo.repo,
                                                             pull_number: pr.number,
                                                             sha: prData.data.head.sha,
+                                                            commit_message: 'Merged by [tjenkinson/gh-action-auto-merge-dependency-updates](https://github.com/tjenkinson/gh-action-auto-merge-dependency-updates)',
                                                         })];
                                                 case 3:
                                                     _a.sent();
