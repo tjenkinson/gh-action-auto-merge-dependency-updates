f110b91a44c21afe04725b50e3a77d43a4c8ae2b
Tom Jenkinson <tom@tjenkinson.me>
2023-02-19 11:37:27 +0000

    update dependencies

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,10 +1,11 @@
 import typescript from 'rollup-plugin-typescript2';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
 
 export default {
   input: 'src/index.ts',
-  plugins: [typescript(), resolve(), commonjs()],
+  plugins: [typescript(), resolve(), commonjs(), json()],
   onwarn: (e) => {
     if (!['CIRCULAR_DEPENDENCY', 'UNUSED_EXTERNAL_IMPORT'].includes(e.code)) {
       throw e;
